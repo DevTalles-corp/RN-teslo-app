696c3b10635f23a3706e0805a1cb8d2cc1e63698
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-08 14:23:16 -0500

    Refactor HomeScreen component

---
+++ src/presentation/layouts/MainLayout.tsx
@@ -0,0 +1,67 @@
+import { useNavigation } from '@react-navigation/native';
+import { Divider, Layout, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { MyIcon } from '../components/ui/MyIcon';
+
+
+interface Props {
+  title: string;
+  subTitle?: string;
+
+  rightAction?: () => void;
+  rightActionIcon?: string;
+
+  children: React.ReactNode;
+}
+
+export const MainLayout = ({
+  title,
+  subTitle,
+  rightAction,
+  rightActionIcon,
+  children,
+}: Props) => {
+
+  const { top } = useSafeAreaInsets();
+  const { canGoBack, goBack } = useNavigation();
+
+
+  const renderBackAction = () => (
+    <TopNavigationAction 
+      icon={ <MyIcon name="arrow-back-outline" /> }
+      onPress={ goBack }
+    />
+  )
+
+  const RenderRightAction = () => {
+
+    if ( rightAction === undefined || rightActionIcon === undefined ) return null;
+
+    return (
+      <TopNavigationAction 
+        onPress={ rightAction }
+        icon={ <MyIcon name={rightActionIcon} /> }
+      />
+    )
+  }
+
+
+
+  return (
+    <Layout style={{ paddingTop: top }}>
+      <TopNavigation 
+        title={ title }
+        subtitle={ subTitle }
+        alignment="center"
+        accessoryLeft={ canGoBack() ? renderBackAction : undefined }
+        accessoryRight={ () => <RenderRightAction /> }
+      />
+      <Divider />
+
+      <Layout style={{ height: '100%' }}>
+        {children}
+      </Layout>
+      
+    </Layout>
+  );
+};
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,28 +1,22 @@
-import { Layout, Text } from '@ui-kitten/components';
-
-import { getProductsByPage } from '../../../actions/products/get-products-by-page';
-import { useQuery } from '@tanstack/react-query';
+import {getProductsByPage} from '../../../actions/products/get-products-by-page';
+import {useQuery} from '@tanstack/react-query';
+import {MainLayout} from '../../layouts/MainLayout';
+import { Text } from '@ui-kitten/components';
 
 
 export const HomeScreen = () => {
-
-  
-
-  const { isLoading, data: products = [] } = useQuery({
-    queryKey:  ['products', 'infinite'],
+  const {isLoading, data: products = []} = useQuery({
+    queryKey: ['products', 'infinite'],
     staleTime: 1000 * 60 * 60, // 1 hour
-    queryFn: () => getProductsByPage(0), 
+    queryFn: () => getProductsByPage(0),
   });
 
-
   return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      
-      <Text>{ JSON.stringify( products, null, 2) }</Text>
-
-
-      
-      
-    </Layout>
-  )
-}
+    <MainLayout
+      title="TesloShop - Products"
+      subTitle="Aplicación administrativa"
+      >
+        <Text>Hola Mundo</Text>
+      </MainLayout>
+  );
+};
