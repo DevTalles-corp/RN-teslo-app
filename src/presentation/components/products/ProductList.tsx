1a9c7a9e5897e9296ece524c6c4d67426cfa4e82
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-09 11:06:56 -0500

    Refactor ProductList component and add pull-to-refresh functionality

--- src/presentation/components/products/ProductList.tsx
+++ src/presentation/components/products/ProductList.tsx
@@ -1,8 +1,10 @@
-import {Layout, List, Text} from '@ui-kitten/components';
-import {Product} from '../../../domain/entities/product';
-import {ProductCard} from './ProductCard';
 import { useState } from 'react';
 import { RefreshControl } from 'react-native';
+import {Layout, List } from '@ui-kitten/components';
+import { useQueryClient } from '@tanstack/react-query';
+
+import {Product} from '../../../domain/entities/product';
+import {ProductCard} from './ProductCard';
 
 interface Props {
   products: Product[];
@@ -11,13 +13,15 @@ interface Props {
 
 export const ProductList = ({products, fetchNextPage}: Props) => {
 
+  const queryClient = useQueryClient();
   const [isRefreshing, setIsRefreshing] = useState(false);
+
 
 
   const onPullToRefresh = async() => {
     setIsRefreshing(true);
-    // Sleep 2
-    await new Promise(resolve => setTimeout(resolve, 1500));
+    await new Promise(resolve => setTimeout(resolve, 200));
+    queryClient.invalidateQueries({ queryKey: ['products', 'infinite'] });
 
     setIsRefreshing(false);
   }
